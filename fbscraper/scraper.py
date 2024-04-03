eadf284ab611b601276d306e7a5a815c6efbf760
RexBanner6000 <tghaynes@gmail.com>
2024-04-03 22:35:58 +0100

    Switch to scraping fields

--- fbscraper/scraper.py
+++ fbscraper/scraper.py
@@ -1,61 +1,29 @@
 from bs4 import BeautifulSoup
+import pandas as pd
 import re
-
-
-class CountryRecord:
-    def __init__(
-        self,
-        country_name: str,
-        country_code: str,
-        base_path: str
-    ):
-        self.country_name = country_name
-        self.country_code = country_code
-        self.country_path = f"{base_path}{country_code}.html"
-        self.population = None
-
-    def scrape_population(self):
-        with open(self.country_path, "r") as fp:
-            soup = BeautifulSoup(fp, "html.parser")
-
-        divs = soup.find_all("div")
-        for div in divs:
-            if re.match("Population:", div.get_text()):
-                table_row = div.find_parent("tr")
-                raw_str = table_row.find_all("td")[-1].get_text()
-                if match := re.search(r"((\d{1,3},?)+)", raw_str):
-                    self.population = int(match.group(0).replace(",", ""))
 
 
 class CIALocalScraper:
     def __init__(
         self,
-        index_html_path: str,
+        base_url: str,
     ):
-        self.index_html_path = index_html_path + "index.html"
-        self.geos_path = index_html_path + "geos/"
+        self.base_url = base_url
 
-    def get_countries(self):
-        country_records = []
-        with open(self.index_html_path, "r") as fp:
+    def get_purchasing_power_parity(self):
+        with open(self.base_url + "fields/2001.html", "r") as fp:
             soup = BeautifulSoup(fp, "html.parser")
-        options = soup.find_all("option")
-        for tag in options:
-            if re.search("select a country", tag.text.lower()) or re.search("world", tag.text.lower()):
-                continue
-            country_records.append(
-                CountryRecord(
-                    country_name=tag.text.lstrip(),
-                    country_code=tag.get("value")[5:7],
-                    base_path=self.geos_path
-                )
-            )
-        return country_records
+        table_rows = [x.find_parent("tr") for x in soup.find_all(string=re.compile("purchasing power parity"))]
+        countries = []
+        gdps = []
+        for row in table_rows:
+            cells = row.find_all("td")
+            if gdp_str := re.findall(r"(?:\$(\d+ \w+))", cells[1].get_text()):
+                countries.append(cells[0].get_text().replace("\n", ""))
+                gdps.append(gdp_str[0])
+        return pd.Series(gdps, index=countries)
 
 
 if __name__ == "__main__":
-    scraper = CIALocalScraper(index_html_path="./data/factbook-2002/")
-    countries = scraper.get_countries()
-    for country in countries:
-        country.scrape_population()
-        print(f"{country.country_name}: {country.population}")
+    scraper = CIALocalScraper(base_url="./data/factbook-2002/")
+    print(scraper.get_purchasing_power_parity())
